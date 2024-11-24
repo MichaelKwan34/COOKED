e9a6a4673526cc3ce8d1b26b62e9ec5cb5b5e449
Emily Zhang <52023695+emily-zhang021@users.noreply.github.com>
2024-11-24 13:57:23 -0800

    Re-commented signed in code

--- app/src/main/java/com/group34/cooked/LoginActivity.kt
+++ app/src/main/java/com/group34/cooked/LoginActivity.kt
@@ -33,6 +33,7 @@ class LoginActivity : AppCompatActivity() {
 
         // If the user has signed in, open the Home page directly instead of log in
         // Note: LOGOUT implementation is needed
+        /*
        val currentUser = firebaseAuth.currentUser
         if (currentUser != null) {
            // If user is already signed in, redirect to HomeActivity
@@ -42,6 +43,7 @@ class LoginActivity : AppCompatActivity() {
            finish()
           return // No need to proceed with the rest of the onCreate code
       }
+      */
 
         email = findViewById(R.id.email_address_input)
         password = findViewById(R.id.password_input)
